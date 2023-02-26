76b5be5758da47a2486b19128f169fbaf9cadb42
Joshua David Sta Rita <jdcstarita@gmail.com>
2023-02-26 09:06:22 +0800

    update item rendering

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -6,7 +6,7 @@ import {AiFillDelete} from "react-icons/ai"
 const ItemCard = ({itemName, price, description, updateMode, deleteItem}) => {
   return (
     <div className='toDoCard'>
-        <div className='itemName'>{itemName}:{price} - {description}</div>
+        <div className='itemName'>{itemName} (${price}) : {description}</div>
         <div className='icons'>
           <BiEdit className='icon' onClick={updateMode} />
           <AiFillDelete className='icon' onClick={deleteItem} />
